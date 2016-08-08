6e3ea0ff7fafd2bd96819d22c5a3c84c0790db9e
NathanGE <nathangelliott@gmail.com>
2016-08-08 19:38:14 -0400

    Added beer model

---
+++ db/migrate/20160808232504_create_beers.rb
@@ -0,0 +1,11 @@
+class CreateBeers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :beers do |t|
+      t.string :name
+      t.float :abv
+      t.integer :ibu
+      
+      t.timestamps
+    end
+  end
+end
